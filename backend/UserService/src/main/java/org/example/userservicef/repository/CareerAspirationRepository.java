38de38e02c38f3fcf733c0a80034e1871f63a8e5
Ismail-Ah <81312910+Ismail-Ah@users.noreply.github.com>
2025-03-12 00:07:12 +0000

    Delete backend/UserService/src/main/java/org/example/userservicef/repository directory

--- backend/UserService/src/main/java/org/example/userservicef/repository/CareerAspirationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.userservicef.Repository;
-
-import org.example.userservicef.Model.CareerAspiration;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface CareerAspirationRepository extends Neo4jRepository<CareerAspiration, String> {
-}
--- backend/UserService/src/main/java/org/example/userservicef/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.userservicef.Repository;
-
-import org.example.userservicef.Model.Subject;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface SubjectRepository extends Neo4jRepository<Subject, String> {
-}
